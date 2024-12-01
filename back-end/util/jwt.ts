3b1ca59a21a1cf4dacdf318ceb876210b45142b1
Baptiste025 <r0982291@ucll.be>
2024-12-01 11:53:33 +0100

    foute code in commentaar verwijderd

--- back-end/util/jwt.ts
+++ back-end/util/jwt.ts
@@ -16,13 +16,3 @@ const generateJwtToken = ({ email, role  }: JwtPayload): string => {
 };
 
 export { generateJwtToken };
-// const generateJwtToken = ({ username }): string => {
-//     const options = { expiresIn: `${process.env.JWT_EXPIRES_HOURS}h`, issuer: 'fitfait'};
-
-//     try  {
-//         return jwt.sign({ username }, process.env.JWT_SECRET as string, options);
-//     } catch (error) {
-//         console.log(error);
-//         throw new Error('Error generating JWT token, see server log for details.');
-//     }
-// }
