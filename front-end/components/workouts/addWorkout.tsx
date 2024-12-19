79af84c4c59a6b5fbe9dabaf44b58228dff617b0
Baptiste025 <r0982291@ucll.be>
2024-12-19 10:31:15 +0100

    laatste vertaling vergeten pushen

--- front-end/components/workouts/addWorkout.tsx
+++ front-end/components/workouts/addWorkout.tsx
@@ -3,13 +3,15 @@ import { useTranslation } from "next-i18next";
 type AddWorkoutsButtonProps = {
     onClick: () => void;
 };
-// const { t } = useTranslation();
 
-const AddWorkoutsButton: React.FC<AddWorkoutsButtonProps> = ({ onClick }) => (
-    <button className="btn btn-primary" onClick={onClick}>
-        {/* {t("stats.addstats")} */}
-        Add workout
-    </button>
-);
+const AddWorkoutsButton: React.FC<AddWorkoutsButtonProps> = ({ onClick }) => {
+    const { t } = useTranslation(); 
+
+    return (
+        <button className="btn btn-primary" onClick={onClick}>
+            {t("workouts.addWorkouts")}
+        </button>
+    );
+};
 
 export default AddWorkoutsButton;
